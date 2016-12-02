746299b16a41f2fff88f0517142b26784afa73b4
qianjianeng <329426491@qq.com>
2016-12-03 00:46:05 +0800

    把扩展中的类方法 改进为 便利构造函数

--- douyu/douyu/Classes/Tools/Category/UIBarButtonItem+Extension.swift
+++ douyu/douyu/Classes/Tools/Category/UIBarButtonItem+Extension.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 extension UIBarButtonItem {
+    
+    /*
     class func itemCreated(imageName:String,highLightImageName:String,size:CGSize) -> UIBarButtonItem {
         let btn = UIButton(type: .custom)
         btn.setImage(UIImage(named:imageName), for: .normal)
@@ -16,4 +18,17 @@ extension UIBarButtonItem {
         btn.frame = CGRect(origin: CGPoint(x:0,y:0), size: size)
         return UIBarButtonItem(customView: btn)
     }
+   */
+    
+    ///便利构造函数
+    /// 1. convenience 开头
+    /// 2. 在构造函数中必须明确调用一个设计的构造函数（self）
+    convenience init(imageName:String,highLightImageName:String,size:CGSize) {
+        let btn = UIButton(type: .custom)
+        btn.setImage(UIImage(named:imageName), for: .normal)
+        btn.setImage(UIImage(named:highLightImageName), for: .highlighted)
+        btn.frame = CGRect(origin: CGPoint(x:0,y:0), size: size)
+        self.init(customView:btn)
+    }
+    
 }
