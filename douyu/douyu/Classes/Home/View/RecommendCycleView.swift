8775e7374a914a02bdbfdb0c60d9d7f3c4f069e3
qianjianeng <329426491@qq.com>
2016-12-31 18:32:27 +0800

    定时器

--- douyu/douyu/Classes/Home/View/RecommendCycleView.swift
+++ douyu/douyu/Classes/Home/View/RecommendCycleView.swift
@@ -17,11 +17,17 @@ class RecommendCycleView: UIView {
     
     @IBOutlet weak var pageController: UIPageControl!
     
+    var timer: Timer?;
+    
     public var cycleArr : [CycleModel]? {
         didSet {
             guard let arr = cycleArr else { return }
             collectionView.reloadData()
             pageController.numberOfPages = arr.count
+            
+            let index = IndexPath(item: (cycleArr?.count)! * 10, section: 0)
+            collectionView.scrollToItem(at: index, at: .left, animated: false)
+            addTimer()
         }
     }
     override func awakeFromNib() {
@@ -37,17 +43,19 @@ class RecommendCycleView: UIView {
         
         
         collectionView.register(UINib(nibName: "RecommendCycleCell", bundle: nil), forCellWithReuseIdentifier: kCycleCellId)
+        
+        
     }
     
 }
 
 extension RecommendCycleView:UICollectionViewDataSource,UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cycleArr?.count ?? 0
+        return (cycleArr?.count ?? 0)*100
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cycle = cycleArr?[indexPath.item]
+        let cycle = cycleArr![(indexPath as NSIndexPath).item % cycleArr!.count]
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCycleCellId, for: indexPath) as? RecommendCycleCell
         
@@ -64,8 +72,20 @@ extension RecommendCycleView:UICollectionViewDataSource,UICollectionViewDelegate
 
 extension RecommendCycleView:UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let currentIndex = Int(scrollView.contentOffset.x / kScreenWidth)
+        
+        let offsetX = scrollView.contentOffset.x + scrollView.bounds.size.width * 0.5
+        
+        let currentIndex = Int(offsetX / collectionView.bounds.size.width) % (cycleArr?.count ?? 1)
+        
         pageController.currentPage = currentIndex
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        removeTimer()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        addTimer()
     }
 }
 
@@ -74,3 +94,25 @@ extension RecommendCycleView {
         return Bundle.main.loadNibNamed("RecommendCycleView", owner: nil, options: nil)?.first as! RecommendCycleView
     }
 }
+
+
+///定时器
+extension RecommendCycleView {
+    func addTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(gotoNext), userInfo: nil, repeats: true)
+    }
+    
+    func removeTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc func gotoNext() {
+        let currentOffsetX = collectionView.contentOffset.x
+        if currentOffsetX > (collectionView.contentSize.width * 4 / 5) {
+            collectionView.setContentOffset(CGPoint(x:collectionView.contentSize.width/2,y:0), animated: false)
+        }
+        collectionView.setContentOffset(CGPoint(x:currentOffsetX+kScreenWidth,y:0), animated: true)
+    }
+}
+
